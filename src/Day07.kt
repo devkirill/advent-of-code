5763c092679ffbf0aad11bfa7655053e1c43d8fb
Deviatkin Kirill <deviatkin-kirill@ya.ru>
2021-12-12 02:02:33 +0500

    day 07

---
+++ src/Day07.kt
@@ -0,0 +1,21 @@
+import kotlin.math.abs
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        val l = input[0].split(",").map(String::toInt)
+        return (0..1000).map { p -> l.sumOf { abs(it - p) } }.minOf { it }
+    }
+
+    fun part2(input: List<String>): Int {
+        val l = input[0].split(",").map(String::toInt)
+        return (0..1000).map { p -> l.sumOf { abs(it - p) * (abs(it - p) + 1) / 2 } }.minOf { it }
+    }
+
+    val testInput = readInput("Day07_test")
+    assertEquals(part1(testInput), 37)
+    assertEquals(part2(testInput), 168)
+
+    val input = readInput("Day07")
+    println(part1(input))
+    println(part2(input))
+}
