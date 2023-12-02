d833fa97020717578f4b235d12b6453b1204c4ab
DevKirill <4599661+devkirill@users.noreply.github.com>
2023-12-02 10:44:56 +0500

    day 2

---
+++ src/main/kotlin/advent/calendar/aoc/solutions/y2023/Day02.kt
@@ -0,0 +1,28 @@
+package advent.calendar.aoc.solutions.y2023
+
+import advent.calendar.aoc.Solution
+import org.springframework.stereotype.Component
+import kotlin.math.max
+
+@Component
+class Day02 : Solution<List<Day02.Game>>() {
+    override fun parse(lines: List<String>) = lines.map { line ->
+        val (a, b) = line.split(": ")
+        val maps = b.split("; ").map { g -> g.split(", ").map { val (n, c) = it.split(" "); c to n.toInt() }.toMap() }
+        val id = a.split(" ").last().toInt()
+        Game(id, maps)
+    }
+
+    override fun part1(input: List<Game>) = input.filter { g ->
+        g.states.all {
+            (it["red"] ?: 0) <= 12 && (it["green"] ?: 0) <= 13 && (it["blue"] ?: 0) <= 14
+        }
+    }.sumOf { it.id }
+
+    override fun part2(input: List<Game>) = input.sumOf { g ->
+        val state = g.states.reduce { a, b -> (a.keys + b.keys).toSet().associateWith { max(a[it] ?: 0, b[it] ?: 0) } }
+        setOf("red", "green", "blue").map { state[it] ?: 0 }.reduce { a, b -> a * b }
+    }
+
+    data class Game(val id: Int, val states: List<Map<String, Int>>)
+}
