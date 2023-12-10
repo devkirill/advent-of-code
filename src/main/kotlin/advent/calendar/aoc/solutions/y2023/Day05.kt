b78f4bac7064ef14c5279ff3657835038dd1f0ed
DevKirill <4599661+devkirill@users.noreply.github.com>
2023-12-10 15:20:58 +0500

    day 5 part 2

--- src/main/kotlin/advent/calendar/aoc/solutions/y2023/Day05.kt
+++ src/main/kotlin/advent/calendar/aoc/solutions/y2023/Day05.kt
@@ -1,33 +1,76 @@
 package advent.calendar.aoc.solutions.y2023
 
 import advent.calendar.aoc.Solution
+import java.lang.Long.min
+import kotlin.math.max
 
 class Day05 : Solution<Day05.Data>() {
     override fun parse(lines: List<String>): Data {
         val data = lines.joinToString("\n").split("\n\n").map { g -> g.split("\n") }
         val seeds = data.first().first().substringAfter(": ").split(" ").map { it.toLong() }
         val ranges = data.drop(1)
-            .map { lines -> lines.drop(1).map { val (a, b, c) = it.split(" ").map { it.toLong() };Range(a, b, c) } }
+            .map { lines ->
+                lines.drop(1).map { val (a, b, c) = it.split(" ").map { it.toLong() };Range(b until (b + c), a - b) }
+            }
         val rr = ranges.map { Mapper(it) }
         return Data(seeds, rr)
     }
 
-    override fun part1(input: Data): Long {
-        return input.seeds.minOf { r ->
-            var it = r
-            for (mapper in input.mappers) {
-                it = mapper[it]
-            }
-            it
+    override fun part1(input: Data) = input.seeds
+        .map { it..it }
+        .flatMap { r ->
+            input.mappers.fold(listOf(r)) { ranges, mapper -> ranges.flatMap { it.apply(mapper) } }
         }
-    }
+        .minOf { it.first }
+
+    override fun part2(input: Data) = input.seeds
+        .chunked(2)
+        .map { it[0] until (it[0] + it[1]) }
+        .flatMap { r ->
+            input.mappers.fold(listOf(r)) { ranges, mapper -> ranges.flatMap { it.apply(mapper) } }
+        }
+        .minOf { it.first }
 
     data class Data(val seeds: List<Long>, val mappers: List<Mapper>)
 
-    data class Mapper(val ranges: List<Range>) {
-        operator fun get(i: Long) =
-            ranges.firstOrNull { i >= it.get && i < it.get + it.size }?.let { it.insert - it.get + i } ?: i
+    data class Mapper(val ranges: List<Range>)
+
+    data class Range(val range: LongRange, val offset: Long) {
+        fun intersect(another: LongRange): Range? {
+            val r = max(range.first, another.first)..min(range.last, another.last)
+            return if (r.first <= r.last) {
+                return Range(r, offset)
+            } else null
+        }
     }
 
-    data class Range(val insert: Long, val get: Long, val size: Long)
+    fun LongRange.apply(mapper: Mapper): List<LongRange> {
+        val result = mutableListOf<LongRange>()
+        mapper.ranges.forEach { range ->
+            val intersect = range.intersect(this)
+            if (intersect != null) {
+                result += intersect.range.apply(intersect)
+            }
+        }
+        result += this.sub(mapper.ranges.map { it.range })
+        return result
+    }
+
+    fun LongRange.apply(range: Range) = (first + range.offset)..(last + range.offset)
+
+    fun LongRange.sub(range: LongRange) =
+        if (max(first, range.first) <= min(last, range.last)) {
+            listOf(first until range.first, (range.last + 1) until last)
+                .filter { it.first <= it.last }
+        } else listOf(this)
+
+    fun LongRange.sub(ranges: List<LongRange>): List<LongRange> {
+        var result = listOf(this)
+        ranges.forEach { range ->
+            result = result.flatMap { it.sub(range) }
+        }
+        return result
+    }
+
+//    data class Range(val insert: Long, val get: Long, val size: Long)
 }
