a02b680c1832e6364f3eb50cd4eebd570357f7e6
devkirill <4599661+devkirill@users.noreply.github.com>
2023-12-29 11:29:44 +0500

    day 19 part 2

--- src/main/kotlin/advent/calendar/aoc/solutions/y2023/Day19.kt
+++ src/main/kotlin/advent/calendar/aoc/solutions/y2023/Day19.kt
@@ -3,6 +3,8 @@ package advent.calendar.aoc.solutions.y2023
 import advent.calendar.aoc.Solution
 import advent.calendar.aoc.solutions.utils.parse
 import advent.calendar.aoc.solutions.utils.splitEmptyLine
+import kotlin.math.max
+import kotlin.math.min
 
 class Day19 : Solution<Day19.Data>() {
     override fun parse(lines: List<String>) = lines
@@ -37,6 +39,33 @@ class Day19 : Solution<Day19.Data>() {
         }
         resolve(part, "in") == "A"
     }.sumOf { it.sum() }
+
+    override fun part2(input: Data): Long {
+        fun resolve(part: List<IntRange>, step: String): Long {
+            if (step == "A") return part.map { it.count().toLong() }.reduce { a, b -> a * b }
+            val rules = input.rules[step] ?: return 0L
+            val p = part.toMutableList()
+            var result = 0L
+            for ((key, sign, value, nextStep) in rules) {
+                if (sign == ">" && p[key].last > value) {
+                    val pp = p.toMutableList()
+                    pp[key] = intersect(p[key], (value + 1)..4000)
+                    p[key] = intersect(p[key], 1..value)
+                    result += resolve(pp, nextStep)
+                }
+                if (sign == "<" && p[key].first < value) {
+                    val pp = p.toMutableList()
+                    pp[key] = intersect(p[key], 1 until value)
+                    p[key] = intersect(p[key], value..4000)
+                    result += resolve(pp, nextStep)
+                }
+            }
+            return result
+        }
+        return resolve(listOf(1..4000, 1..4000, 1..4000, 1..4000), "in")
+    }
+
+    private fun intersect(a: IntRange, b: IntRange) = max(a.first, b.first)..min(a.last, b.last)
 
     data class Data(val rules: Map<String, List<Rule>>, val parts: List<List<Int>>)
 
