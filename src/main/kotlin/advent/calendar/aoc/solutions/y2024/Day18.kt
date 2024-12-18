ebcd1b773912fa4a714994ea1a40a2608cbfaf4b
devkirill <4599661+devkirill@users.noreply.github.com>
2024-12-18 11:41:55 +0500

    day 18

--- src/main/kotlin/advent/calendar/aoc/solutions/y2024/Day18.kt
+++ src/main/kotlin/advent/calendar/aoc/solutions/y2024/Day18.kt
@@ -12,7 +12,7 @@ class Day18 : Solution<List<Point>>() {
     override fun parse(lines: List<String>) =
         lines.map { it.parse("(\\d+),(\\d+)".toRegex()) { it.toInt() }.let { (x, y) -> Point(x, y) } }
 
-    fun find(
+    fun solve(
         input: List<Point>,
         bytes: Int = (if (input.size > 1024) 1024 else 12),
         size: Int = (if (input.size > 1024) 70 else 6)
@@ -20,23 +20,18 @@ class Day18 : Solution<List<Point>>() {
         val walls = input.take(bytes).toSet()
         val result =
             dijkstraSearch(listOf(Point(0, 0)), { it.nearby4().filter { it !in walls && it in 0..size } }, { 1 })
-//        val grid = Grid<String>(Point(size + 1, size + 1), walls.associate { it to "#" }) { "." }
-//        println("$bytes")
-//        println(grid)
         return result[Point(size, size)]
     }
 
     override fun part1(input: List<Point>): Any {
-        return find(input)!!
+        return solve(input)!!
     }
 
     override fun part2(input: List<Point>): Any {
-        val cache = mutableMapOf<Int, Int?>()
-        fun find(bytes: Int) = cache.computeIfAbsent(bytes) { find(input, it) }
         val res = input.indices.toList().binarySearchBy(0) {
             when {
-                find(it) != null -> -1
-                find(it - 1) == null -> 1
+                solve(input, it) != null -> -1
+                solve(input, it - 1) == null -> 1
                 else -> 0
             }
         }
